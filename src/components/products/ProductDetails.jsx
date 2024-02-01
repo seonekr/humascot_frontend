c0d55bf8cf1c3e369a8cb36ab194536ebca519ce
seonekr <zaemuevln42gmail.com>
2024-02-01 17:54:31 +0700

    Review product display

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -1,28 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./productDetails.css";
 import Header from "../header/Header";
 import Menu from "../menuFooter/Menu";
-import { IoIosArrowBack } from "react-icons/io";
 import { BiStore } from "react-icons/bi";
-import icon_star from "../../img/icon_star.png";
-import icon_star2 from "../../img/icon_star2.png";
-import user from "../../img/user.png";
-import productImage from "../../img/productImage.png";
-import detailproduct from "../../img/detailproduct.jpg";
 import axios from "axios";
+import user from "../../img/user.png";
 
 function ProductDetails() {
   const { goods_id } = useParams();
   const [goods_list, set_goods_list] = useState([]);
-  const [reiews_list, set_reviews_list] = useState([]);
-  const [sliceNum, set_sliceNum] = useState(8);
-  const [category, set_category] = useState(6);
-  const storage = JSON.parse(window.localStorage.getItem("user"));
-
-  console.log(goods_list)
-
+  const [reviews, setReviews] = useState([]);
   const [product, setProduct] = useState(null);
+  // Rating
+  const [comment, setComment] = useState({
+    review: "",
+    star: 0,
+  });
+  const user_id = JSON.parse(window.localStorage.getItem("user")).user_id;
 
   useEffect(() => {
     const config = {
@@ -44,15 +39,11 @@ function ProductDetails() {
       });
   }, [goods_id]);
 
-  const handleMore = () => {
-    set_sliceNum(sliceNum + 8);
-  };
-
   useEffect(() => {
     let config = {
       method: "get",
       maxBodyLength: Infinity,
-      url: import.meta.env.VITE_API + `/store/?category=${category}`,
+      url: import.meta.env.VITE_API + `/store/?goods`,
       headers: {
         "Content-Type": "application/json",
       },
@@ -66,21 +57,151 @@ function ProductDetails() {
       .catch((error) => {
         console.log(error);
       });
-  }, [category]);
+  }, []);
 
-  console.log(product);
+  // Get review product
+  useEffect(() => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: import.meta.env.VITE_API + `/store/review/${goods_id}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        setReviews(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  // Number of product
+  const [goodsCount, setGoodsCount] = useState(1);
+
+  // Reduce the number of products
+  const incrementCount = (goodsID) => {
+    setGoodsCount((prevCounts) => ({
+      ...prevCounts,
+      [goodsID]: (prevCounts[goodsID] || 1) + 1,
+    }));
+  };
+
+  // Increase the number of products
+  const decrementCount = (goodsID) => {
+    setGoodsCount((prevCounts) => ({
+      ...prevCounts,
+      [goodsID]: Math.max(1, (prevCounts[goodsID] || 1) - 1),
+    }));
+  };
+
+  const handleRatingChange = (newStar) => {
+    setComment({ ...comment, star: newStar });
+  };
+
+  // Comment input box
+  const handleChange = (e) => {
+    if (comment.star < 1) {
+      e.preventDefault();
+      return;
+    }
+    setComment({ ...comment, [e.target.name]: e.target.value });
+    autoGrowTextarea(e.target);
+  };
+
+  // Resize grow up if typing full box
+  const autoGrowTextarea = () => {
+    const textarea = document.getElementById("multiline-input");
+
+    if (textarea) {
+      const maxHeight = (10 * window.innerHeight) / 100;
+      textarea.style.height = "auto";
+      textarea.style.height = Math.min(textarea.scrollHeight, maxHeight) + "px";
+    }
+  };
+
+  // sent review
+  const [loading, setLoading] = useState(true);
+
+  // const handleReviewSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   try {
+  //     const newReview = {
+  //       product: product.id,
+  //       rating: comment.rating,
+  //       ...comment,
+  //     };
+
+  //     console.log(newReview)
+
+  //     setComment({
+  //       comment: "",
+  //       rating: 0,
+  //     });
+
+  //     document.getElementById("review").style.height = "auto";
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  // Handle review
+  const handleReview = (e) => {
+    e.preventDefault();
+
+    const data = {
+      user_id: parseInt(user_id),
+      goods_id: product.id,
+      star: parseInt(comment.star),
+      ...comment,
+    };
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: import.meta.env.VITE_API + "/store/review/",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+    setComment({
+      review: "",
+      star: 0,
+    });
+    console.log(data);
+    document.getElementById("review").style.height = "auto";
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
       <Header />
       <div className="contentBody">
         {/* <Link to="/" className="box_container_back_icons_back">
-          <IoIosArrowBack id="icons_back" />
-          <p>Back</p>
-        </Link> */}
+            <IoIosArrowBack id="icons_back" />
+            <p>Back</p>
+          </Link> */}
 
         <div className="box_betavinOfob">
-          {/* {joinedData.map((item) => ( */}
           {product ? (
             <div>
               <form className="boxProduct_deteils">
@@ -89,7 +210,7 @@ function ProductDetails() {
                 </div>
                 <div className="txtContentproduct">
                   <h1 className="txt_nameP">{product.name}</h1>
-                  <p className="money_txt">{product.price} $</p>
+                  <p className="money_txt">{product.price} Kip</p>
                   <p className="txt_description">{product.description}</p>
                   <Link to="/stores" className="store_boxLink">
                     <BiStore className="iconn_linkbox" />
@@ -97,76 +218,70 @@ function ProductDetails() {
                   </Link>
 
                   {/* <div className="size_product">
-                    <p>Size:</p>
-                    {item.size && (
-                      <div className="size">
-                        {item.size.map((size, index) => (
-                          <p className="echSize" key={index}>
-                            {size}
-                          </p>
-                        ))}
-                      </div>
-                    )}
-                  </div>
+                        <p>Size:</p>
+                        {item.size && (
+                          <div className="size">
+                            {item.size.map((size, index) => (
+                              <p className="echSize" key={index}>
+                                {size}
+                              </p>
+                            ))}
+                          </div>
+                        )}
+                      </div> */}
                   <div className="container_item_icon">
                     <div
                       className="container_minus_plus"
-                      onClick={() => decrementCount(item.id)}
+                      onClick={() => decrementCount(product.id)}
                     >
                       -
                     </div>
-                    <span>{goodsCount[item.id] || 1}</span>
+                    <span>{goodsCount[product.id] || 1}</span>
                     <div
                       className="container_minus_plus"
-                      onClick={() => incrementCount(item.id)}
+                      onClick={() => incrementCount(product.id)}
                     >
                       +
                     </div>
                   </div>
                   <div className="Count_product">
-                    <Link onClick={buyNow} className="echbtn btnBut">
-                      Buy Now
-                    </Link>
-                    <Link onClick={addToCart} className="echbtn btnAdd">
-                      Add To Cart
-                    </Link>
-                  </div> */}
+                    <Link className="echbtn btnBut">Buy Now</Link>
+                    <Link className="echbtn btnAdd">Add To Cart</Link>
+                  </div>
                 </div>
               </form>
             </div>
           ) : (
             <p>Loading...</p>
           )}
-          {/* ))} */}
 
           {/* <div className="description_container">
-            <img src={detailproduct} alt="" />
-          </div> */}
+              <img src={detailproduct} alt="" />
+            </div> */}
 
-          {/* <div className="review_box">
-            <h3>Reviews(8)</h3>
-
-            <form className="review_boxstar" onSubmit={handleReviewSubmit}>
+          <div className="review_box">
+            <h3>Reviews ({reviews && reviews.length})</h3>
+            <form className="review_boxstar" onSubmit={handleReview}>
               <div className="star">
                 {[1, 2, 3, 4, 5].map((star) => (
                   <span
                     key={star}
                     style={{
                       cursor: "pointer",
-                      color: star <= comment.rating ? "#FFBF00" : "gray",
+                      color: star <= comment.star ? "#FFBF00" : "gray",
                     }}
                     onClick={() => handleRatingChange(star)}
                   >
-                    {star <= comment.rating ? "★" : "☆"}
+                    {star <= comment.star ? "★" : "☆"}
                   </span>
                 ))}
               </div>
               <div className="comment_box_content">
                 <textarea
-                  name="commend"
+                  name="review"
                   className="review-text"
                   id="review"
-                  value={comment.commend}
+                  value={comment.review}
                   onChange={handleChange}
                   placeholder="Your opinion"
                   maxLength="255"
@@ -174,7 +289,7 @@ function ProductDetails() {
                 />
                 <button
                   type="submit"
-                  disabled={comment.rating < 1 || comment.commend.trim() === ""}
+                  disabled={comment.star < 1 || comment.review.trim() === ""}
                   className="btn_submit_comment"
                 >
                   Sent
@@ -188,22 +303,23 @@ function ProductDetails() {
                     <p>Loading reviews...</p>
                   ) : (
                     <div>
-                      {reviews.map((item, index) => (
-                        <div className="box_comment_connntent" key={item.id}>
+                      {reviews.map((review, index) => (
+                        <div className="box_comment_connntent" key={index}>
                           <div className="box_head_user">
                             <div className="box_head_user_title">
                               <div className="comment__userImg">
-                                <img src={user} alt="" />
+                                {/* Assuming `user` is the path to the user image */}
+                                <img src={user} alt="user" />
                               </div>
                               <div className="box_head_user_titt_detils">
-                                <p>{item.userid}</p>
+                                <p>UserID: {review.user}</p>
                                 <div className="review_boxstar_user">
-                                  {[...Array(item.rating)].map((_, index) => (
+                                  {[...Array(review.star)].map((_, index) => (
                                     <span key={index} className="star">
                                       ★
                                     </span>
                                   ))}
-                                  {[...Array(5 - item.rating)].map(
+                                  {[...Array(5 - review.star)].map(
                                     (_, index) => (
                                       <span
                                         key={index}
@@ -218,11 +334,11 @@ function ProductDetails() {
                               </div>
                             </div>
                             <div className="box_user_time">
-                              <p>2023.08.23</p>
+                              <p>{review.created_at}</p>
                             </div>
                           </div>
                           <div className="comment_boxOfuser">
-                            <p>{item.commend}</p>
+                            <p>{review.review}</p>
                           </div>
                         </div>
                       ))}
@@ -232,40 +348,32 @@ function ProductDetails() {
               ) : (
                 <p className="no-available">No reviews available.</p>
               )}
-
-              <div className="comment_boxOfuser">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                  numquam sapiente voluptates ut porro quisquam eveniet voluptas
-                  sed. Nulla ducimus odit esse quam corporis, dolorem labore.
-                  Ipsa quis repudiandae nihil.
-                </p>
-              </div>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
-      <h2 className="box_betavinOfob asd2">
-        <span className="spennofStyle"> </span>
-        More products
-      </h2>
-      <div className="product-area">
-        {goods_list.map((i, index) => (
-          <div className="box-product" key={index} >
-            <Link to={"/goods/"+i.id}>
-              <div className="img">
-                <img src={i.image} alt="image" />
-              </div>
-              <ul className="txtOFproduct2">
-                <li className="name">{i.name}</li>
-                <li className="price">{i.price}</li>
-                <li className="desc">{i.description}</li>
-              </ul>
-            </Link>
-          </div>
-        ))}
-
-      </div>
+      <section id="more-product">
+        <h2 className="box_betavinOfob asd2">
+          <span className="spennofStyle"> </span>
+          More products
+        </h2>
+        <div className="product-area-details">
+          {goods_list.map((i, index) => (
+            <div className="box-product-details" key={index}>
+              <Link to={"/goods/" + i.id} target="/#endregion">
+                <div className="img-details">
+                  <img src={i.image} alt="image" />
+                </div>
+                <ul className="txtOFproduct2-details">
+                  <li className="name">{i.name}</li>
+                  <li className="price">{i.price}</li>
+                  <li className="desc">{i.description}</li>
+                </ul>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </section>
       <Menu />
     </>
   );
